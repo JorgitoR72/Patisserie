9e1b929d952921ffb96f67792c0598dc4fcc24b9
Alvaro Martin <martinhuertaalvaro@gmail.com>
2023-05-18 10:26:25 +0200

    correccion de error

--- admin.php
+++ admin.php
@@ -120,19 +120,12 @@ if (count($_POST) > 0) {
           <div class="col-md-12 d-lg-none">
             <nav class="navbar navbar-dark bg">
               <div class="container-fluid">
-<<<<<<< HEAD
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-controls="collapseWidthExample" aria-expanded="false" aria-label="Toggle navigation">
-=======
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                  data-bs-target="#collapseWidthExample" aria-controls="collapseWidthExample" aria-expanded="false"
-                  aria-label="Toggle navigation">
->>>>>>> Alvaro
                   <span class="navbar-toggler-icon"></span>
                 </button>
               </div>
             </nav>
           </div>
-<<<<<<< HEAD
           <img class="d-none d-lg-block" id="boton-toggle" src="img/Logotipo Restaurante.png" alt="Logo" style="padding-bottom: 20px;">
           <div class="collapse.show" id="collapseWidthExample">
             <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
@@ -146,29 +139,7 @@ if (count($_POST) > 0) {
                 <img src="img/recetas.png" alt="" style="padding-right: 25px; width: 75px;">RECETAS
               </button>
               <br>
-              <a class="nav-link text-start" id="v-pills-exit-tab" href="salir.html" role="tab" aria-selected="false"><img src="img/salida.png" alt="" style="padding-right: 25px;">SALIR</a>
-=======
-          <img class="d-none d-lg-block" id="boton-toggle" src="img/Logotipo Restaurante.png" alt="Logo"
-            style="padding-bottom: 20px;">
-          <div class="collapse.show" id="collapseWidthExample">
-            <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-              <a class="nav-link text-star" id="v-pills-home-tab" href="index.php" role="tab"
-                aria-selected="false"><img src="img/casa1.svg" alt="" style="padding-right: 25px;">INICIO</a>
-              <br>
-              <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile"
-                type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false"
-                style="text-align: left;">
-                <img src="img/chef.png" alt="" style="padding-right: 25px; width: 75px;">PERFIL
-              </button>
-              <br>
-              <button class="nav-link" id="v-pills-exit-tab" data-bs-toggle="pill" data-bs-target="#v-pills-exit"
-                type="button" role="tab" aria-controls="v-pills-exit" aria-selected="false" style="text-align: left;">
-                <img src="img/recetas.png" alt="" style="padding-right: 25px; width: 75px;">RECETAS
-              </button>
-              <br>
-              <a class="nav-link text-start" id="v-pills-exit-tab" href="exit.php" role="tab"
-                aria-selected="false"><img src="img/salida.png" alt="" style="padding-right: 25px;">SALIR</a>
->>>>>>> Alvaro
+              <a class="nav-link text-start" id="v-pills-exit-tab" href="exit.php" role="tab" aria-selected="false"><img src="img/salida.png" alt="" style="padding-right: 25px;">SALIR</a>
             </div>
           </div>
         </div>
@@ -192,12 +163,7 @@ if (count($_POST) > 0) {
                   <br>
                   <h2>SUSCRIPCION</h2>
                   <div class="dropdown">
-<<<<<<< HEAD
                     <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-=======
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
-                      data-bs-toggle="dropdown" aria-expanded="false">
->>>>>>> Alvaro
                       Seleccionar plan
                     </button>
                     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
@@ -217,7 +183,6 @@ if (count($_POST) > 0) {
                 </div>
               </div>
             </div>
-<<<<<<< HEAD
             <div class="tab-pane fade show active" id="v-pills-exit" role="tabpanel" aria-labelledby="v-pills-exit-tab">
               <div class="container" style="padding: 0;">
                 <h1>RECETAS</h1>
@@ -296,174 +261,15 @@ if (count($_POST) > 0) {
                           <button type="submit" class="btn btn-primary">Guardar</button>
                         </div>
                       </form>
-=======
-            <div class="tab-pane fade" id="v-pills-exit" role="tabpanel" aria-labelledby="v-pills-exit-tab">
-              <div class="container">
-                <h1>RECETAS</h1>
-                <table class="table">
-                  <thead>
-                    <tr>
-                      <th>IdReceta</th>
-                      <th>Nombre</th>
-                      <th>Plan</th>
-                      <th>Imagen</th>
-                      <th>Acciones</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>1</td>
-                      <td>pastel</td>
-                      <td>Vip</td>
-                      <td>img1.png</td>
-                      <td>
-                        <button class="btn btn-primary btn-sm" data-bs-toggle="modal"
-                          data-bs-target="#editarModal">Editar</button>
-                        <button class="btn btn-danger btn-sm" data-bs-toggle="modal"
-                          data-bs-target="#confirmDeleteModal">Eliminar</button>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>2</td>
-                      <td>tarta</td>
-                      <td>Basico</td>
-                      <td>img2.png</td>
-                      <td>
-                        <button class="btn btn-primary btn-sm" data-bs-toggle="modal"
-                          data-bs-target="#editarModal">Editar</button>
-                        <button class="btn btn-danger btn-sm" data-bs-toggle="modal"
-                          data-bs-target="#confirmDeleteModal">Eliminar</button>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>3</td>
-                      <td>pan</td>
-                      <td>Basico</td>
-                      <td>img3.png</td>
-                      <td>
-                        <button class="btn btn-primary btn-sm" data-bs-toggle="modal"
-                          data-bs-target="#editarModal">Editar</button>
-                        <button class="btn btn-danger btn-sm" data-bs-toggle="modal"
-                          data-bs-target="#confirmDeleteModal">Eliminar</button>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>1</td>
-                      <td>pastel</td>
-                      <td>Vip</td>
-                      <td>img1.png</td>
-                      <td>
-                        <button class="btn btn-primary btn-sm" data-bs-toggle="modal"
-                          data-bs-target="#editarModal">Editar</button>
-                        <button class="btn btn-danger btn-sm" data-bs-toggle="modal"
-                          data-bs-target="#confirmDeleteModal">Eliminar</button>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-
-              <!-- Modal de confirmación para eliminar -->
-              <div class="modal fade" id="confirmDeleteModal" tabindex="-1" aria-labelledby="confirmDeleteModalLabel"
-                aria-hidden="true">
-                <div class="modal-dialog">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title" id="confirmDeleteModalLabel">Confirmar eliminación</h5>
-                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                      <p>¿Estás seguro de que deseas eliminar esta receta?</p>
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                      <button type="button" class="btn btn-danger">Eliminar</button>
->>>>>>> Alvaro
                     </div>
                   </div>
                 </div>
               </div>
 
-<<<<<<< HEAD
-=======
-              <!-- Modal de Edición -->
-              <div class="modal fade" id="editarModal" tabindex="-1" aria-labelledby="editarModalLabel"
-                aria-hidden="true">
-                <div class="modal-dialog">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title" id="editarModalLabel">Editar Receta</h5>
-                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                      <div class="container">
-                        <form>
-                          <div class="form-group">
-                            <label for="nombreReceta">Nombre de la Receta:</label>
-                            <input type="text" class="form-control" id="nombreReceta"
-                              placeholder="Ingrese el nombre de la receta" required>
-                          </div>
-                          <div class="form-group">
-                            <label for="preparacion">Preparación:</label>
-                            <textarea class="form-control" id="preparacion" rows="3"
-                              placeholder="Ingrese los pasos de preparación" required></textarea>
-                          </div>
-                          <div class="form-group">
-                            <label for="descripcion">Descripción:</label>
-                            <textarea class="form-control" id="descripcion" rows="3"
-                              placeholder="Ingrese una descripción breve" required></textarea>
-                          </div>
-                          <div class="form-group">
-                            <label for="ingredientes">Ingredientes:</label>
-                            <div id="ingredientes">
-                              <div class="input-group mb-3">
-                                <input type="text" class="form-control" placeholder="Nombre del ingrediente" required>
-                                <input type="text" class="form-control" placeholder="Descripción del ingrediente"
-                                  required>
-                                <div class="input-group-append">
-                                  <button class="btn btn-primary" type="button"
-                                    onclick="agregarIngrediente()">Agregar</button>
-                                  <button class="btn btn-danger" type="button"
-                                    onclick="quitarIngrediente()">Quitar</button>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="form-group">
-                            <label for="plan">Plan al que pertenece:</label>
-                            <select class="form-control" id="plan" required>
-                              <option value="">Seleccione un plan</option>
-                              <option value="1">Plan 1</option>
-                              <option value="2">Plan 2</option>
-                              <option value="3">Plan 3</option>
-                            </select>
-                          </div>
-                          <div class="form-group">
-                            <label for="urlVideo">URL del Video:</label>
-                            <input type="text" class="form-control" id="urlVideo"
-                              placeholder="Ingrese la URL del video">
-                          </div>
-                          <div class="form-group">
-                            <label for="urlImagen">URL de la Imagen:</label>
-                            <input type="text" class="form-control" id="urlImagen"
-                              placeholder="Ingrese la URL de la imagen">
-                          </div>
-                        </form>
-                      </div>
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                      <button type="button" class="btn btn-primary">Guardar</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
->>>>>>> Alvaro
 
             </div>
           </div>
         </div>
-<<<<<<< HEAD
 
       </div>
     </div>
@@ -507,14 +313,6 @@ if (count($_POST) > 0) {
     });
   </script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-=======
-      </div>
-    </div>
-  </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-    crossorigin="anonymous"></script>
->>>>>>> Alvaro
 </body>
 
 </html>
